17a2d276bd56d9b35e28dbf832372e756264a0d1
baier2u <geoffrey.baier7@etu.univ-lorraine.fr>
2018-02-13 20:22:05 +0100

    Ajout de la création des tables de la base de données

--- database/auth.php
+++ database/auth.php
@@ -4,10 +4,15 @@ use Illuminate\Database\Capsule\Manager;
 use Illuminate\Database\Schema\Blueprint;
 
 $dropTables = [
-    'user'
+    'user',
+    'edibility',
+    'biotope',
+    'trophic_status',
+    'specie'
 ];
 
 foreach ($dropTables as $table) {
+    Manager::schema()->disableForeignKeyConstraints();
     Manager::schema()->dropIfExists($table);
 }
 
@@ -20,4 +25,51 @@ Manager::schema()->create('user', function (Blueprint $table) {
     $table->string('first_name')->nullable();
     $table->text('role');
     $table->timestamps();
+    $table->index('id');
+});
+
+Manager::schema()->create('edibility', function (Blueprint $table) {
+    $table->engine = 'InnoDB';
+    $table->increments('id');
+    $table->string('status')->unique();
+    $table->timestamps();
+    $table->index('id');
+});
+
+Manager::schema()->create('biotope', function (Blueprint $table) {
+    $table->engine = 'InnoDB';
+    $table->increments('id');
+    $table->string('region')->unique();
+    $table->timestamps();
+    $table->index('id');
+});
+
+Manager::schema()->create('trophic_status', function (Blueprint $table) {
+    $table->engine = 'InnoDB';
+    $table->increments('id');
+    $table->string('status')->unique();
+    $table->timestamps();
+    $table->index('id');
+});
+
+Manager::schema()->create('specie', function (Blueprint $table) {
+    $table->engine = 'InnoDB';
+    $table->increments('id');
+    $table->string('name_latin')->unique();
+    $table->string('name_french')->unique();
+    $table->unsignedInteger('edibility_id');
+    $table->unsignedInteger('biotope_id');
+    $table->unsignedInteger('trophic_status_id');
+    $table->timestamps();
+    $table->index('id');
+    $table->index('name_latin');
+    $table->index('edibility_id');
+    $table->index('biotope_id');
+    $table->index('trophic_status_id');
+});
+
+Manager::schema()->table('specie', function(Blueprint $table) {
+    $table->foreign('edibility_id')->references('id')->on('edibility')->onDelete('cascade')->onUpdate('cascade');
+    $table->foreign('biotope_id')->references('id')->on('biotope')->onDelete('cascade')->onUpdate('cascade');
+    $table->foreign('trophic_status_id')->references('id')->on('trophic_status')->onDelete('cascade')->onUpdate('cascade');
 });
