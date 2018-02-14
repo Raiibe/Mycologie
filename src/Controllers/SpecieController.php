35ae90b8828690291b8753396274a1bdee1ad7b5
baier2u <geoffrey.baier7@etu.univ-lorraine.fr>
2018-02-15 00:31:59 +0100

    Ajout de la pagination sur la liste des champignons

--- src/Controllers/SpecieController.php
+++ src/Controllers/SpecieController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Models\Specie;
+use App\Utils\Paginator;
 use App\Utils\Session;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
@@ -12,9 +13,16 @@ class SpecieController extends BaseController
     public function index(RequestInterface $request, ResponseInterface $response)
     {
         $species = Specie::get();
+        $perPage = 10;
+        $offset = ($perPage * (!is_null($request->getParam('page')) ? ($request->getParam('page') - 1) : 0));
+        $total = count($species);
+        $species = Specie::limit($perPage)
+            ->offset($offset)
+            ->get();
 
         $this->render($response, 'specie/index', [
-            'species' => $species
+            'species' => $species,
+            'pagination' => Paginator::paginate($perPage, $total, $request->getParam('page'))
         ]);
     }
 }
