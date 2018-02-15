1b65d8a491840087b94a9f8128dc92c1ca14534e
baier2u <geoffrey.baier7@etu.univ-lorraine.fr>
2018-02-15 18:18:51 +0100

    Ajout du triage de la liste des champignons

--- src/Config/app.conf.php
+++ src/Config/app.conf.php
@@ -1,13 +1,5 @@
 <?php
 
-/**
- * template.config.php
- *
- * Application configuration
- *
- * @author     WILMOUTH Steven
- * @version    1
- */
 return array(
     'name' => 'Mycologie', // Name displayed in html head
 
